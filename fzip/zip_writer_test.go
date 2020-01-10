990a22bb01ea3bbc9fe15cd4ee1667d8b0904de6
go-follow <gitshmelcer@gmail.com>
2020-01-10 20:32:58 +0700

    Тесты для zip_writer

--- fzip/zip_writer_test.go
+++ fzip/zip_writer_test.go
@@ -32,5 +32,63 @@ func TestZip(t *testing.T) {
 	if err := Zip(listNameReader, fileZip); err != nil {
 		t.Fatal(err)
 	}
+}
 
+func TestZipFiles(t *testing.T) {
+	fileZip, err := os.Create("test.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fileZip.Close()
+	defer os.Remove("test.zip")
+
+	f1, err := os.OpenFile("1.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f2, err := os.OpenFile("2.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f3, err := os.OpenFile("3.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove("1.txt")
+	defer os.Remove("2.txt")
+	defer os.Remove("3.txt")
+
+	if err := ZipFiles([]*os.File{f1, f2, f3}, fileZip); err != nil {
+		t.Fatal(err)
+	}
+}
+
+
+func TestZipPath(t *testing.T) {
+	fileZip, err := os.Create("test.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fileZip.Close()
+	//defer os.Remove("test.zip")
+
+	_, err = os.OpenFile("1.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.OpenFile("2.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.OpenFile("3.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove("1.txt")
+	defer os.Remove("2.txt")
+	defer os.Remove("3.txt")
+
+	if err := ZipPath([]string{"1.txt", "2.txt", "3.txt"}, "test.zip", false); err != nil {
+		t.Fatal(err)
+	}
 }

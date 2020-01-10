7c26e124eb4771fe8f40d66938e30fcc06252699
go-follow <gitshmelcer@gmail.com>
2020-01-10 20:53:44 +0700

    test for UnZipPath

--- fzip/zip_reader_test.go
+++ fzip/zip_reader_test.go
@@ -2,7 +2,6 @@ package fzip
 
 import (
 	"os"
-	"fmt"
 	"testing"
 )
 
@@ -34,9 +33,48 @@ func TestUnZipFile(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	r, err := UnZipFile(fileZip)
+	_, err = UnZipFile(fileZip)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(r)
+}
+
+func TestUnZipPath(t *testing.T) {
+	fileZip, err := os.Create("test.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fileZip.Close()
+
+	f1, err := os.OpenFile("1.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f2, err := os.OpenFile("2.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f3, err := os.OpenFile("3.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove("1.txt")
+	defer os.Remove("2.txt")
+	defer os.Remove("3.txt")
+
+	if err := ZipFiles([]*os.File{f1, f2, f3}, fileZip); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := UnZipPath("test.zip", "..", false); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := UnZipPath("test.zip", "..", true); err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove("../1.txt")
+	defer os.Remove("../2.txt")
+	defer os.Remove("../3.txt")
 }

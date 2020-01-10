1dc223dcc16cf725ac3d61a012cb5ab6e55dbbde
go-follow <gitshmelcer@gmail.com>
2020-01-10 19:56:46 +0700

    ZipPath

--- fzip/zip_writer.go
+++ fzip/zip_writer.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 //NameReader структура необходимая для метода Zip.
@@ -14,6 +15,10 @@ type NameReader struct {
 	Name string
 	//Reader - содержимое файла
 	Reader io.Reader
+}
+
+type listPathFiles struct {
+	list []string
 }
 
 //Zip создание zip архива по массиву NameReader
@@ -48,6 +53,148 @@ func ZipFiles(files []*os.File, zipFile io.Writer) error {
 	defer writerZip.Close()
 
 	for _, file := range files {
+		f, err := writerZip.Create(file.Name())
+		if err != nil {
+			return err
+		}
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		_, err = f.Write(data)
+		if err != nil {
+			return err
+		}
+		if err := file.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//ZipPath создание zip архива по массиву путей до файлов
+func ZipPath(pathFiles []string, pathZip string, deleteFiles ...bool) error {
+	listFiles := make([]*os.File, 0)
+
+	listStringFiles := &listPathFiles{
+		list: []string{},
+	}
+
+	for _, p := range pathFiles {
+		file, err := os.Open(p)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		fileInfo, err := file.Stat()
+		if err != nil {
+			return err
+		}
+
+		if !fileInfo.IsDir() {
+			listFiles = append(listFiles, file)
+			continue
+		}
+		if err := getFilesInDir(p, listStringFiles); err != nil {
+			return err
+		}
+	}
+
+	if err := checkUniqueFilesName(listFiles); err != nil {
+		return err
+	}
+
+	zipFile, err := os.Create(pathZip)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	listDirs, err := fillFilesFromStringDirs(listStringFiles.list)
+	if err != nil {
+		return err
+	}
+
+	if err := zipFilesFromDir(listFiles, listDirs, zipFile); err != nil {
+		return err
+	}
+
+	//удаление файлов добавленных в архив
+	if len(deleteFiles) > 0 && deleteFiles[0] {
+		for _, f := range pathFiles {
+			if err := os.RemoveAll(f); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func getFilesInDir(pathDir string, files *listPathFiles) error {
+	filesInfo, err := ioutil.ReadDir(pathDir)
+	if err != nil {
+		return err
+	}
+	for _, f := range filesInfo {
+		if !f.IsDir() {
+			t := filepath.Join(pathDir, f.Name())
+			files.list = append(files.list, t)
+			continue
+		}
+		if err := getFilesInDir(filepath.Join(pathDir, f.Name()), files); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func checkUniqueFilesName(listFiles []*os.File) error {
+	mapList := make(map[string]bool)
+	for _, file := range listFiles {
+		if mapList[filepath.Base(file.Name())] {
+			return fmt.Errorf("file %v must be unique at the root of the archive",
+				filepath.Base(file.Name()))
+		}
+		mapList[filepath.Base(file.Name())] = true
+	}
+	return nil
+}
+
+func fillFilesFromStringDirs(listPathDirs []string) ([]*os.File, error) {
+	listDirs := make([]*os.File, 0)
+
+	for _, f := range listPathDirs {
+		file, err := os.Open(f)
+		if err != nil {
+			return nil, err
+		}
+		listDirs = append(listDirs, file)
+	}
+	return listDirs, nil
+}
+
+func zipFilesFromDir(listFiels []*os.File, listDirs []*os.File, zipFile io.Writer) error {
+	writerZip := zip.NewWriter(zipFile)
+	defer writerZip.Close()
+
+	for _, file := range listFiels {
+		f, err := writerZip.Create(filepath.Base(file.Name()))
+		if err != nil {
+			return err
+		}
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		_, err = f.Write(data)
+		if err != nil {
+			return err
+		}
+		if err := file.Close(); err != nil {
+			return err
+		}
+	}
+	for _, file := range listDirs {
 		f, err := writerZip.Create(file.Name())
 		if err != nil {
 			return err

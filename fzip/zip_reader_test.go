4edec346840079d22d263cc9fa1555f01d04549e
go-follow <gitshmelcer@gmail.com>
2020-01-10 20:38:11 +0700

    test for UnZipFile

--- fzip/zip_reader_test.go
+++ fzip/zip_reader_test.go
@@ -1,7 +1,42 @@
 package fzip
 
-import "testing"
+import (
+	"os"
+	"fmt"
+	"testing"
+)
 
 func TestUnZipFile(t *testing.T) {
-	
+	fileZip, err := os.Create("test.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fileZip.Close()
+	defer os.Remove("test.zip")
+
+	f1, err := os.OpenFile("1.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f2, err := os.OpenFile("2.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f3, err := os.OpenFile("3.txt", os.O_CREATE, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove("1.txt")
+	defer os.Remove("2.txt")
+	defer os.Remove("3.txt")
+
+	if err := ZipFiles([]*os.File{f1, f2, f3}, fileZip); err != nil {
+		t.Fatal(err)
+	}
+
+	r, err := UnZipFile(fileZip)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(r)
 }
